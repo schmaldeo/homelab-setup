df56db3d4c7672cbc923b0c1495d0fdd67b6e7f4
Michał Skrzyszowski <mskrzyszowski@outlook.com>
2026-09-13 16:50:04 +0200

    Quote first argument in cp

--- scripts/bk-integrity-checker/check.sh
+++ scripts/bk-integrity-checker/check.sh
@@ -6,7 +6,7 @@ STATUS=$?
 sudo losetup -d "$LOOP_DEV"
 
 if [ $STATUS -eq 0 ] || [ $STATUS -eq 1 ]; then
-        cp $1 "$1_checked"
+        cp "$1" "$1_checked"
         echo "Check succeeded"
 else
         curl -d "IMPORTANT: Corrupt backup detected" "ntfy.sh/$NTFY_TOPIC"
